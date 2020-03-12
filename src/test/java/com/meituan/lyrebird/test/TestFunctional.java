ab54d8af60d4b02d2bdca98fc48668cb6262a971
reeyur <lee.beyond90@gmail.com>
2020-03-12 17:56:02 +0800

    修改Codacy/PR Quality Review

--- src/test/java/com/meituan/lyrebird/test/TestFunctional.java
+++ src/test/java/com/meituan/lyrebird/test/TestFunctional.java
@@ -177,11 +177,11 @@ public class TestFunctional {
                 "{\"code\": 1000,\"data\": {\"id\": \"cfa0c589-8ef0-4885-b4f4-b9688c5af0d5\", \"name\": \"test-data\", \"response\": {\"data\": \"[{\\\"type\\\": \\\"scheme\\\", \\\"info\\\":{\\\"value\\\": \\\"test://www.lyrebird.java.sdk.com\\\"}, \\\"desc\\\": \\\"The scheme of target page\\\"}]\"}}, \"message\": \"success\"}"));
 
         LBMockData lbMockData = this.lyrebird.getMockData("cfa0c589-8ef0-4885-b4f4-b9688c5af0d5");
-        Assert.assertEquals("cfa0c589-8ef0-4885-b4f4-b9688c5af0d5", lbMockData.getId());
-        Assert.assertEquals("test-data", lbMockData.getName());
+        assertEquals("cfa0c589-8ef0-4885-b4f4-b9688c5af0d5", lbMockData.getId());
+        assertEquals("test-data", lbMockData.getName());
 
         List<String> urlScheme = JsonPath.parse(lbMockData.getResponseData().toString()).read("$[?(@.type == 'scheme')].info.value");
-        Assert.assertEquals(1, urlScheme.size());
-        Assert.assertEquals("test://www.lyrebird.java.sdk.com", urlScheme.get(0));
+        assertEquals(1, urlScheme.size());
+        assertEquals("test://www.lyrebird.java.sdk.com", urlScheme.get(0));
     }
 }
